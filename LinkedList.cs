44b03ab9fedbafca122ee8682a38400d1a889c88
Antoshyn4 <dmytro.antoshyn@nure.ua>
2021-04-01 12:12:11 +0300

    Создал Класс LinkedList и к нему поля Первого элемента, последнего и длина списка

---
+++ LinkedList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class LinkedList
+{
+    public Node<T> first { get; private set; }
+    public Node<T> last { get; private set; }
+
+    public int Length { get; private set; }
+
+}
